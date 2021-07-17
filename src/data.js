05b019919d53dad9e6cf6721058b38c0319f2336
Ritesh <ritesh19e@gmail.com>
2021-07-17 10:26:38 +0530

    New upadte

---
+++ src/data.js
@@ -0,0 +1,18 @@
+export default [
+	{ id: '1', imageName: 'img1.jpg', tag: 'Flower' },
+	{ id: '2', imageName: 'img2.jpg', tag: 'Trees' },
+	{ id: '3', imageName: 'img3.jpg', tag: 'Art/Craft' },
+	{ id: '4', imageName: 'img4.jpg', tag: 'Road' },
+	{ id: '5', imageName: 'img5.jpg', tag: 'Flower' },
+	{ id: '6', imageName: 'img6.jpg', tag: 'Nature' },
+	{ id: '7', imageName: 'img7.jpg', tag: 'Animal' },
+	{ id: '8', imageName: 'img8.jpeg', tag: 'Nature' },
+	{ id: '9', imageName: 'img9.jpeg', tag: 'Art/Craft' },
+	{ id: '10', imageName: 'img10.jpeg', tag: 'Road' },
+	{ id: '11', imageName: 'img11.jpeg', tag: 'Trees' },
+	{ id: '12', imageName: 'img12.jpeg', tag: 'Nature' },
+	{ id: '13', imageName: 'img13.jpeg', tag: 'Flower' },
+	{ id: '14', imageName: 'img14.jpeg', tag: 'Art/Craft' },
+	{ id: '15', imageName: 'img15.jpeg', tag: 'Road' },
+	{ id: '16', imageName: 'img16.jpeg', tag: 'Animal' }
+];
